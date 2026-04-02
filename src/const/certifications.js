23d81d4c0348cb35b95326d025d37cfb73c605c9
GeorgeContreras241 <maicolcontreras241@gmail.com>
2026-04-02 17:20:24 -0500

    feat(data): expand certifications data with new entries

---
+++ src/const/certifications.js
@@ -0,0 +1,34 @@
+const certifications = [
+    {
+        id: 1,
+        name: 'Tecnología en Desarrollo de Software',
+        description: 'Universidad de Cundinamarca',
+        date: '2024 - Culminado',
+        image: './logo/udec-logo.png',
+        url: undefined,
+        github: undefined,
+        state: true
+    },
+    {
+        id: 2,
+        name: 'BootCamp Desarrollo Web Full Stack',
+        description: 'Telento Tech',
+        date: '2024 - Culminado',
+        image: './logo/telento-tech.png',
+        url: 'https://certificados.talentotech.co/?cert=2387056631&fingerprint=y#pdf',
+        github: 'https://github.com/GeorgeContreras241',
+        state: true
+    },
+    {
+        id: 3,
+        name: 'Analisis y Desarrolo de software',
+        description: 'SENA - Servicio Nacional de Aprendizaje',
+        date: '2026 - Actual',
+        image: './logo/logo-sena.webp',
+        url: 'https://www.sena.edu.co/',
+        github: undefined,
+        state: true
+    }
+];
+
+export default certifications;
