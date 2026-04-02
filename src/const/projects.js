e33902c0fa55c22ef4da0b62fbec0c7cfdb1b48e
GeorgeContreras241 <maicolcontreras241@gmail.com>
2026-04-02 16:56:16 -0500

    fix(projects): correct spanish grammar and ortography in project descriptions

--- src/const/projects.js
+++ src/const/projects.js
@@ -1,17 +1,16 @@
 export const projects = [
     {
       id: 1,
-      name: 'Nube - Gestor de Contraseñas',
+      name: 'Nube y Local con Autenticación WebAuthn',
       description:
-        'Aplicación Full Stack segura para gestionar contraseñas. Las contraseñas se encriptan en el frontend antes de almacenarse en la base de datos, garantizando que solo el usuario pueda desencriptarlas con su clave maestra.',
+        'Aplicación que permite gestionar contraseñas de forma segura con autenticación WebAuthn de manera local o en la nube.',
       technologies: [
-        { name: 'React', icon: 'React' },
+        { name: 'NextJS', icon: 'NextJS' },
         { name: 'Tailwind', icon: 'Tailwind' },
-        { name: 'ExpressJS', icon: 'Express' },
         { name: 'MongoDB', icon: 'Mongo' }
       ],
       url: null,
-      github: 'https://github.com/GeorgeContreras241/G-Password_manager',
+      github: 'https://github.com/GeorgeContreras241/G--Project-Sena',
       image: './images/project07.webp',
       state: true
 
@@ -34,7 +33,7 @@ export const projects = [
       id: 4,
       name: 'Ward Spy',
       description:
-        'Aplicacion Web que permite al usuario a travez del riotTag de League of Legends buscar informacion de sus partidas y live game. Gestionando la API de Riot Games a travez de NextJS y cacheando la informacion para que no se sobrecargue el servidor. Mediante uso de MongoDB y localStorage. ',
+        'Aplicación web que permite al usuario a través del Riot Tag de League of Legends buscar información de sus partidas y live games. Gestionando la API de Riot Games a través de NextJS y cacheando la información para que no se sobrecargue el servidor. Mediante uso de MongoDB y localStorage.',
       technologies: [
         { name: 'NextJS', icon: 'NextJS' },
         { name: 'Tailwind', icon: 'Tailwind' },
@@ -44,19 +43,5 @@ export const projects = [
       github: 'https://github.com/GeorgeContreras241/Ward-Spy',
       image: './images/project09.webp',
       state: true
-    },
-    {
-      id: 5,
-      name: 'Carrito de Compras',
-      description:
-        'Ejercicio de carrito de compras con React y Tailwind CSS.',
-      technologies: [
-        { name: 'React', icon: 'React' },
-        { name: 'Tailwind CSS', icon: 'Tailwind' }
-      ],
-      url: "https://store-api-sage.vercel.app/",
-      github: 'https://github.com/GeorgeContreras241/G-Store_Api',
-      image: './images/project04.webp',
-      state: false
     }
   ];
